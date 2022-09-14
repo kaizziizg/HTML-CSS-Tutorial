b296511e813aa69cd8c985b55544fa871e4f412b
kaizz <63266693+kaizziizg@users.noreply.github.com>
2022-09-14 22:52:53 +0800

    add preloadImg & fix Target Page url

--- Week1/js/xinbao.js
+++ Week1/js/xinbao.js
@@ -3,6 +3,33 @@ let currnetImg = 0;
 let xinBaoElement = document.getElementById("xinbao");
 let xinBaoTimesElement = document.getElementById("xinbaoTimes");
 let xinbaoTimes = 0;
+
+var images = [];
+
+var images = [];
+function preload() {
+    for (var i = 0; i < arguments.length; i++) {
+        images[i] = new Image();
+        images[i].src = preload.arguments[i];
+    }
+}
+
+//-- usage --//
+preload(
+    "./img/xinbao-0000.jpg",
+    "./img/xinbao-0001.jpg",
+	"./img/xinbao-0002.jpg",
+	"./img/xinbao-0003.jpg",
+	"./img/xinbao-0004.jpg",
+	"./img/xinbao-0005.jpg",
+	"./img/xinbao-0006.jpg",
+	"./img/xinbao-0007.jpg",
+	"./img/xinbao-0008.jpg",
+	"./img/xinbao-0009.jpg",
+)
+
+
+
 function changeImg() {
 	if (currnetImg > 9) currnetImg = 0;
 	let imgsrc = "./img/xinbao-000" + currnetImg + ".jpg";
